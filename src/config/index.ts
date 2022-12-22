559d61dfead9eb6cb960e8625fc300e819e70aad
keshavdulal <Keshav.dulal@gmail.com>
2022-12-22 12:20:07 +0545

    setup: environment config

---
+++ src/config/index.ts
@@ -0,0 +1,28 @@
+import merge from "lodash.merge";
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const stage = process.env.STAGE || "local";
+
+let envConfig;
+
+if (stage === "produciton") {
+  envConfig = require("./prod").default;
+} else if (stage === "testing") {
+  envConfig = require("./testing").default;
+} else {
+  envConfig = require("./local").default;
+}
+
+const defaultConfig = {
+  stage,
+  env: process.env.NODE_ENV,
+  port: 3001,
+  secrets: {
+    jwt: process.env.JWT_SECRET,
+    dbUrl: process.env.DATABASE_URL,
+  },
+  logging: false,
+};
+
+export default merge(defaultConfig, envConfig);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import config from "./config";
 import app from "./server";
 
-const PORT = 3001;
+// const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`Hello on http://localhost:${PORT}`);
+app.listen(config.port, () => {
+  console.log(`Hello on http://localhost:${config.port}`);
 });
